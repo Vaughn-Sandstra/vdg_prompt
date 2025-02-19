1f3c03718908701fde3c68ab4d049765c983e4ee
Vaughn-Sandstra <Vaughns855@stemcollegiate.ca>
2025-02-19 08:56:53 -0700

    Create feb 19th.cs

---
+++ feb 19th.cs
@@ -0,0 +1,11 @@
+// what did you learn? (feb19th)
+// i learned how to add a variable when coding.
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
