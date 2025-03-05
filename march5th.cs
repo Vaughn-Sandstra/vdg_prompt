b3b75d3a22f13fa45e7ff2eab7140f9202345be4
Vaughn-Sandstra <Vaughns855@stemcollegiate.ca>
2025-03-05 09:27:35 -0700

    Create march5th.cs

---
+++ march5th.cs
@@ -0,0 +1,9 @@
+Console.WriteLine("ive learned how to create a litteral value and print them. also i learned what bool means. i know what a float, double, and decimal are.");
+Console.WriteLine('b');
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine("123");
+Console.WriteLine(123);
+Console.WriteLine("true");
+Console.WriteLine(true);
